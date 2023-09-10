48ea918023783995984c70aebc1d5f2ba73341c8
Patrick Bonayon <bonayonpatrick@example.com>
2023-09-10 17:28:23 +0800

    code clean-up fixed some bugs

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ import setuptools
 setuptools.setup(
     name='SpotLoad',
     version='1.0',
-    py_modules=[],
     entry_points={
     'console_scripts': [
         'spotload = spotload.__main__:run',
