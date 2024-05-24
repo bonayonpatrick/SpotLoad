07efa03ff99f6667972c65ba90bed82396598c9a
Patrick Bonayon <bonayonpatrick@example.com>
2024-05-24 19:55:41 +0800

    changed youtube music api changed arguments

--- spotload/providers.py
+++ spotload/providers.py
@@ -106,7 +106,7 @@ def search_query(query, auto=False, use_yt=False, delta=10):
     metadata = track["metadata"]
 
     if (video := choose_from_youtube_music(
-        query=query,
+        query=f"{concat_comma(metadata['artist'])} - {metadata['title']}",
         duration=track["duration"],
         delta=delta,
         auto=auto,
